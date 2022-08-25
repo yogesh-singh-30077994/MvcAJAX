9960a8bfcf1cc25a91271d262550fc85662b7f6c
yogesh-singh-30077994 <yogesh.singh@adani.com>
2022-08-25 11:13:01 +0530

    Add project files.

---
+++ MvcAJAX/Controllers/AjaxController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcAJAX.Data;
+using MvcAJAX.Models;
+using MvcAJAX.ViewModels;
+
+namespace MvcAJAX.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AjaxController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> Search(string Title)
+        {
+            var movie = await _context.Movie.Where(m => m.Title!.Contains(Title)).ToListAsync();
+            return Json(movie);
+        }
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> GetMovies()
+        {
+            var movie = await _context.Movie.ToListAsync();
+            return Json(movie);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Create([Bind("Id","Title", "ReleaseDate", "Genre")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return Json(movie);
+            }
+            return new JsonResult("Failed");
+        }
+    }
+}
---
+++ MvcAJAX/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcAJAX.Models;
+
+namespace MvcAJAX.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcAJAX.Models.Movie> Movie { get; set; } = default!;
+    }
+}
